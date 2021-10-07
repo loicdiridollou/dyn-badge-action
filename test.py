05f1f71c519925fc0276235206f0c54ebe1ae861
Loic Diridollou <loic.diridollou@gmail.com>
2021-10-06 22:29:10 -0400

    fnure;

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-url = 'https://api.github.com/gists/{}'.format(os.environ.get('gist_id'))
-message, color, value = os.environ.get('message'), os.environ.get('color'), os.environ.get('value')
+url = 'https://api.github.com/gists/{}'.format(os.environ.get('INPUT_GIST_ID'))
+message, color, value = os.environ.get('INPUT_MESSAGE'), os.environ.get('INPUT_COLOR'), os.environ.get('INPUT_VALUE')
 string_json = '{"schemaVersion": 1, "label": "%s", "message": "%s", "color": "%s"}' % (message, value, color)
 
 print(os.environ)
@@ -10,6 +10,6 @@ print(url, message, color, value, string_json)
 
 myobj = {"public": False, "files": {"coverage.json": {"content": string_json}}}
 
-x = requests.patch(url, json=myobj, auth=('loicdiridollou', os.environ.get('secret')))
+x = requests.patch(url, json=myobj, auth=('loicdiridollou', os.environ.get('INPUT_SECRET')))
 
 print(x.text)
