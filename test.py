617871a5113158e8c3ce95a54d52e081f7877753
Loic Diridollou <loic.diridollou@gmail.com>
2021-10-06 22:10:07 -0400

    bu ob

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-import sys
 import os
+import requests
 
-print('Hello World!')
+url = 'https://api.github.com/gists/{}'.format(os.environ.get('INPUT_gist_id')  )
+message, color, value = os.environ.get('INPUT_message'), os.environ.get('INPUT_color'), os.environ.get('INPUT_value')
+string_json = '{"schemaVersion": 1, "label": "%s", "message": "%s", "color": "%s"}' % (message, value, color)
 
-for arg in sys.argv:
-    print(arg)
+myobj = {"public": False, "files": {"coverage.json": {"content": string_json}}}
 
-print(os.environ.get('INPUT_TEST'))
+x = requests.patch(url, json=myobj, auth=('loicdiridollou', os.environ.get('INPUT_secret')))
+
+print(x.text)
