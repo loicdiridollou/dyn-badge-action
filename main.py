170194986ea5492077cf88b79f2d96c4b70595ed
Loic Diridollou <loic.diridollou@gmail.com>
2021-10-13 22:02:00 -0700

    Deleting test.py to create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Module to update the content of the gist json that defines the badge (badge
+defined by the user using the gist_id argument
+"""
+import os
+import requests
+
+url = 'https://api.github.com/gists/{}'.format(os.environ.get('INPUT_GIST_ID'))
+message, color, value = os.environ.get('INPUT_MESSAGE'), os.environ.get('INPUT_COLOR'), os.environ.get('INPUT_VALUE')
+string_json = '{"schemaVersion": 1, "label": "%s", "message": "%s", "color": "%s"}' % (message, value, color)
+
+myobj = {"public": False, "files": {"coverage.json": {"content": string_json}}}
+
+x = requests.patch(url, json=myobj, auth=(os.environ.get('INPUT_USER'), os.environ.get('INPUT_SECRET')))
+
+print(x.text)
