4551ba2cc2dbfcf5f4269412c0c8aacde1efb562
Loic Diridollou <loic.diridollou@gmail.com>
2021-10-15 20:11:56 -0700

    Extra extension not needed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ url = 'https://api.github.com/gists/{}'.format(os.environ.get('INPUT_GIST_ID'))
 message, color, value = os.environ.get('INPUT_MESSAGE'), os.environ.get('INPUT_COLOR'), os.environ.get('INPUT_VALUE')
 string_json = '{"schemaVersion": 1, "label": "%s", "message": "%s", "color": "%s"}' % (message, value, color)
 
-myobj = {"public": False, "files": {os.environ.get('INPUT_FILENAME')+".json": {"content": string_json}}}
+myobj = {"public": False, "files": {os.environ.get('INPUT_FILENAME'): {"content": string_json}}}
 
 x = requests.patch(url, json=myobj, auth=(os.environ.get('INPUT_USER'), os.environ.get('INPUT_SECRET')))
 
